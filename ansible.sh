6c71866395b4e97966ac95709c599ed79dfcb963
Yosef Salmalian <flaslnm@gmail.com>
2022-08-22 11:40:09 +0430

    add my servers

--- ansible.sh
+++ ansible.sh
@@ -4,3 +4,5 @@ sudo apt update
 sudo apt install python3-pip python3-dev
 python3 -m pip install -U pip
 python3 -m pip install --user ansible==5.6.0
+
+# ansible-playbook -i inventories/hosts.ini clients-etc-hosts.yaml --ask-vault-pass
